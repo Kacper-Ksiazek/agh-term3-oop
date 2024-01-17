1aedb06f3a1c4d3256d73920ccf4b3d87360c564
Kacper Książek <58363032+Kacper-Ksiazek@users.noreply.github.com>
2024-01-17 01:34:35 +0100

    🛠️ jakies niezacomittowane zmiany w szkole_prywatnej

--- szkola_prywatna/main.cpp
+++ szkola_prywatna/main.cpp
@@ -17,85 +17,19 @@ void printWorkersCollection(Worker **workers);
 
 
 int main() {
-    Worker *workersCollection[9] = {nullptr};
+    Worker *workers[9] = {nullptr};
 
-    workersCollection[0] = new Teacher(
-            "Jan Kowalski",
-            30,
-            9000,
-            30,
-            POLSKI,
-            "4Tic"
-    );
+    workers[0] = new Teacher("Koper Adam", 45, 40000, 0, MATEMATYKA);
+    workers[1] = new Teacher("Banan Karolina", 34, 4000, 6, NIEMIECKI);
+    workers[2] = new Teacher("Kowalski Karol", 29, 3000, 1, POLSKI);
+    workers[3] = new Teacher("Nowak Klaudia", 51, 9000, 23, ANGIELSKI, "3b");
+    workers[4] = new Teacher("Wiadro Zbigniew", 60, 8000, 32, MATEMATYKA, "1c");
+    workers[5] = new Teacher("Gruszka Anna", 35, 4000, 8, NIEMIECKI);
+    workers[6] = new Admin("Ogorek Halina", 61, 6000, 26, STARSZY_SPECJALISTA);
+    workers[7] = new Admin("Piotrowski Jan", 43, 4000, 15, SPECJALISTA);
+    workers[8] = new Admin("Wasowski Henryk", 35, 3000, 5, MLODSZY_SPECJALISTA);
 
-    workersCollection[1] = new Teacher(
-            "Piotr Wisniewski",
-            35,
-            2400,
-            28,
-            ANGIELSKI
-    );
-
-    workersCollection[2] = new Teacher(
-            "Michal Lewandowski",
-            40,
-            10000,
-            32,
-            CHEMIA,
-            "5D"
-    );
-
-    workersCollection[3] = new Admin(
-            "Adam Kowalski",
-            30,
-            6500,
-            30,
-            MLODSZY_SPECJALISTA
-    );
-
-    workersCollection[4] = new Teacher(
-            "Anna Nowak",
-            28,
-            8500,
-            25,
-            MATEMATYKA
-    );
-
-    workersCollection[5] = new Teacher(
-            "Katarzyna Dabrowska",
-            32,
-            1400,
-            27,
-            FIZYKA,
-            "1C"
-    );
-
-    workersCollection[6] = new Admin(
-            "Adam Nowak",
-            28,
-            8500,
-            25,
-            SPECJALISTA
-    );
-
-    workersCollection[7] = new Admin(
-            "Kacper Wisniewski",
-            35,
-            4700,
-            28,
-            STARSZY_SPECJALISTA
-    );
-
-    workersCollection[8] = new Teacher(
-            "Magdalena Zajac",
-            29,
-            8700,
-            26,
-            INFORMATYKA
-    );
-
-
-    printWorkersCollection(workersCollection);
+    printWorkersCollection(workers);
 
     return 0;
 }
